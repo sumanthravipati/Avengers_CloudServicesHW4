d32c4baa302c5d6c4e0d72c48a2531b7c557b7c2
sumanthravipatirr <sumanthravipatirr@gmail.com>
2019-03-31 00:12:36 -0700

    Avengers Cloud Services HW4.

---
+++ CloudServicesAssignment4_AvengersTeam/src/main/java/com/avengers/assignment4/CloudServicesAssignment4AvengersTeamApplication.java
@@ -0,0 +1,13 @@
+package com.avengers.assignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServicesAssignment4AvengersTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServicesAssignment4AvengersTeamApplication.class, args);
+	}
+
+}
---
+++ CloudServicesAssignment4_AvengersTeam/src/test/java/com/avengers/assignment4/CloudServicesAssignment4AvengersTeamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avengers.assignment4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudServicesAssignment4AvengersTeamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
